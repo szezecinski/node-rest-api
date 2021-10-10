4745bb63d919d0867edae9e5da79ddfba797cc9f
unknown <szezecinski1@gmail.com>
2021-10-09 22:14:36 -0300

    tiny refactor

--- mysql.js
+++ mysql.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql')
 
 //Tips for mysql troubleshooting ==>  https://github.com/mysqljs/mysql
-
 var pool = mysql.createPool({
     "connectionLimit": process.env.MYSQL_LIMIT,
     "user": process.env.MYSQL_USER,
@@ -10,6 +9,8 @@ var pool = mysql.createPool({
     "host": process.env.MYSQL_HOST,
     "port": process.env.MYSQL_PORT
 })
+
+exports.pool = pool;
 
 exports.execute = (query, params = []) => {
     return new Promise((resolve, reject) => {
@@ -22,5 +23,3 @@ exports.execute = (query, params = []) => {
         })
     })
 }
-
-exports.pool = pool;
